42b80d97dd7121e5cbeffde48c75a1b542d576de
Gael <GaelDeMercado@gmail.com>
2024-05-05 20:53:54 -0700

    Table Mochis

--- frontend/src/components/Client/Sucursales/TableCallsHistory/TableCallsHistory.jsx
+++ frontend/src/components/Client/Sucursales/TableCallsHistory/TableCallsHistory.jsx
@@ -5,12 +5,34 @@ import './TableCallsHistory.scss'
 
 export function TableCallsHistory(props) {
   const { id } = props
+
   return (
     <Table className="table-calls-history-client">
-      <Table.Row>
-        <Table.HeaderCell>Sucursal</Table.HeaderCell>
-        <Table.HeaderCell>Sucursal</Table.HeaderCell>
-      </Table.Row>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Ext. Llama</Table.HeaderCell>
+          <Table.HeaderCell>Ext. Recibe</Table.HeaderCell>
+          <Table.HeaderCell>Inicio</Table.HeaderCell>
+          <Table.HeaderCell>Terminacion</Table.HeaderCell>
+          <Table.HeaderCell>Duracion</Table.HeaderCell>
+          <Table.HeaderCell>Fecha</Table.HeaderCell>
+          <Table.HeaderCell>Llamada Atendida</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>101</Table.Cell>
+          <Table.Cell>202</Table.Cell>
+          <Table.Cell>22:01:00</Table.Cell>
+          <Table.Cell>22:20:00</Table.Cell>
+          <Table.Cell>19:00</Table.Cell>
+          <Table.Cell>01-06-2024</Table.Cell>
+          <Table.Cell className="status">
+            Atendida
+            <Icon name="check" />
+          </Table.Cell>
+        </Table.Row>
+      </Table.Body>
     </Table>
   )
 }
